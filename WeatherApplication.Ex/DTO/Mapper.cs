82317b09e01d64dccf2c9002f01fce4bc060e995
Isidoro <isidoro.rizzo@itsincom.it>
2025-02-19 15:48:23 +0100

    Added a mapper (entity to dto and viceversa)

---
+++ WeatherApplication.Ex/DTO/Mapper.cs
@@ -0,0 +1,45 @@
+﻿using WeatherApplication.Ex.Entities;
+
+namespace WeatherApplication.Ex.DTO
+{
+    public class Mapper
+    {
+        public UserDTO MapEntityToDto(User entity)
+        {
+            return new UserDTO
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+                Email = entity.Email
+            };
+        }
+        public User MapDtoToEntity(UserDTO user)
+        {
+            return new User
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Email = user.Email
+            };
+        }
+
+        public CityDTO MapEntityToDto(City entity)
+        {
+            return new CityDTO
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+                Country = entity.Country
+            };
+        }
+        public City MapDtoToEntity(CityDTO city)
+        {
+            return new City
+            {
+                Id = city.Id,
+                Name = city.Name,
+                Country = city.Country
+            };
+        }
+    }
+}
